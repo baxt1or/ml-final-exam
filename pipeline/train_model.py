aa675d15239a1f3b107fcadc3616cc93dce9b2f5
baxt1or <145246800+baxt1or@users.noreply.github.com>
2025-12-12 12:04:41 +0500

    Cleaning and Tokenizing

--- pipeline/train_model.py
+++ pipeline/train_model.py
@@ -1,14 +1,22 @@
+import pandas as pd
+import numpy as np 
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F 
+import torch.optim as optim
 
 
-vocab_size = 20000 
+vocab_size=30000
 
 batch_size = 64
-block_size = 256
+block_size = 80 # number of tokens in one sentence
+learning_rate = 3e-4
+
 
 max_iters = 5000
 eval_iters = 200
 eval_interval = 500
-learning_rate = 3e-4
+
 
 n_embd = 84
 n_head = 4
@@ -16,3 +24,4 @@ n_layer = 4
 dropout = 0.2
 
 
+X, y, tokenizer = get_token_data()
